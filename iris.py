88a994ce16e02e0d43ead19f2a86f619e680d497
Agata Chmielowiec <kmieluu>
2019-04-26 14:03:23 +0100

    'update'

---
+++ iris.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+
+data = np.genfromtxt('https://github.com/kmieluu/Fisher-s-Iris-Data-Set---PandS-2019-Project/blob/master/Input/iris.data',delimiter=',') 
+print(data)
